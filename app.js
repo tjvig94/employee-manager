e4c6ec9c2c30f5030662a729d6aef757887802ba
Tim Vigneau <tjvig94@gmail.com>
2021-03-11 17:53:36 -0500

    app initializes with main menu

--- app.js
+++ app.js
@@ -12,4 +12,34 @@ const connection = mysql.createConnection({
 connection.connect(err => {
     if (err) throw err;
     console.log("Connected.");
+    init();
 });
+
+const init = () => {
+    inquirer
+        .prompt({
+            name: "action",
+            type: "list",
+            message: "Welcome! What would you like to do: ",
+            choices: [
+                "View all staff",
+                "View staff by department",
+                "View staff by manager"
+            ]
+        })
+        .then(input => {
+            switch(input.action) {
+                case "View all staff":
+                    viewAll();
+                    break;
+                case "View staff by department":
+                    byDepartment();
+                    break;
+                case "View staff by manager":
+                    byManager();
+                    break;
+                default:
+                    console.log("Hmm something's not working...");
+            };
+        });
+};
